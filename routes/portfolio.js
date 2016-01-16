3e00857405a8b952a523dbd871b604cc60d61f68
Karim Bellioum <kbellioum@gmail.com>
2016-01-16 19:53:35 +0000

    adding MongoDB

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://127.0.0.1/mydb');
+
+var Parents = require('../models/parents');
+
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('portfolio', { title: 'portfolio 1 Page', name: 'test'});
+  Parents.find(function(err, parents){
+    res.render('portfolio', { title: 'portfolio 1 Page', name: 'test', parents: parents});
+  });
 });
 
 module.exports = router;
