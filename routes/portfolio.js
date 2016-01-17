98bc0c2c2b072f9971871c451f8844409df0b0c0
Karim Bellioum <kbellioum@gmail.com>
2016-01-17 01:32:03 +0000

    React on

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -10,6 +10,20 @@ var Parents = require('../models/parents');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+
+  var c = "";
+
+  for(i = 1; i <= 100; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        c += " [" + i + "]" + " (tree & five) - ";
+    } else if (i % 3 == 0){
+           c += " [" + i + "]" + " (tree) - ";
+    } else if (i % 5 == 0){
+      c += " [" + i + "]" + " (five) - ";
+    } else {
+      c += i + " - ";
+    }
+  }
   Parents.find(function(err, parents){
     res.render('portfolio', { title: 'portfolio 1 Page', name: 'test', parents: parents});
   });
